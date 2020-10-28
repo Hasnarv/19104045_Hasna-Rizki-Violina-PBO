628a9ce1a84293538e828c7d53a20077327f7c92
hasnarv <hasna.violina@gmail.com>
2020-10-28 15:22:32 +0700

    Menambahkan Tugas dan Praktikum pada Pertemuan ke 1

---
+++ src/com/Hasna/Pertemuan1/Praktikum/Kelas.java
@@ -0,0 +1,17 @@
+package com.Hasna.Pertemuan1.Praktikum;
+
+public class Kelas {
+    //Deklarasi variable
+    private static String nama = "Paimons";
+
+    private static int setMinute() {
+        int minute = 10;
+        return minute;
+    }
+
+    public static void main(String[] args) {
+        //Output
+        System.out.println("Nama\t\t: " + nama);
+        System.out.println("Set Minute\t: " + setMinute());
+    }
+}
