60b703a1cd5c82c0eb85ef5206ee7802f13d925f
Hasnarv <72425456+Hasnarv@users.noreply.github.com>
2020-11-29 10:26:08 +0700

    Praktikum & Tugas Praktikum 7

---
+++ src/com/Hasna/Pertemuan7/Praktikum/deklarasi.js
@@ -0,0 +1,10 @@
+//membuat fungsi dengan deklarasi
+function add(num1, num2){
+    return num1 + num2
+}
+
+//memanggil fungsi
+var result = add(29, 43)
+
+//output
+console.log('hasil dari 29 + 43 = ', result)
